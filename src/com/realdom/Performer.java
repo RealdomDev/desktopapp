260bfc7e434101283c8583ef4dab254f5ff6239a
azhulogic <43792633+azhulogic@users.noreply.github.com>
2018-10-30 21:35:17 -0500

    Revert "Add files via upload"
    This reverts commit 6d0c0d16f7307b4eab2c35a6bd90e3dca4fb6f73.

--- src/com/realdom/Performer.java
+++ src/com/realdom/Performer.java
@@ -1,39 +1,5 @@
 package com.realdom;
 
-import java.awt.Point;
-
 public class Performer {
-    private int idnum;
-    private String label; // perhaps label and idnum can be consolidated
-    private String name; // optional?
-    private String instrument; // optional?
-    private Point[] posarray; // use doubles
 
-    public Performer(int length) {
-        idnum = 0;
-        label = "defaultlabel";
-        name = "defaultname";
-        instrument = "defaultinstrument";
-        posarray = new Point[length];
-    }
-
-    public Performer(int length, int idnum, String label) {
-        this.idnum = idnum;
-        this.label = label;
-        name = "defaultname";
-        instrument = "defaultinstrument";
-        posarray = new Point[length];
-    }
-
-    public Point getPos(int idx) {
-        // I'm not actually sure what .getLocation() does
-        // Java documentation says it "returns location" but perhaps
-        // it may be more useful to write two seperate methods to get x and y
-
-        return posarray[idx].getLocation();
-    }
-
-    public void setPos(int idx, double x, double y) {
-        posarray[idx].setLocation(x, y);
-    }
 }
