7385269fe857c6c9d933eb439400101e4b2fac96
azhulogic <43792633+azhulogic@users.noreply.github.com>
2018-11-28 14:59:48 -0600

    foundational performer class structure, findCount methods

--- src/com/realdom/Performer.java
+++ src/com/realdom/Performer.java
@@ -7,14 +7,22 @@ public class Performer {
     private String label; // perhaps label and idnum can be consolidated
     private String name; // optional?
     private String instrument; // optional?
-    private Point[] posarray; // use doubles
+
+    private Point[] posarray;
+    private boolean[] keyframe;
 
     public Performer(int length) {
         idnum = 0;
         label = "defaultlabel";
         name = "defaultname";
         instrument = "defaultinstrument";
+
         posarray = new Point[length];
+        keyframe = new boolean[length];
+
+        for (int i = 0; i < length; i += 1) {
+            keyframe[i] = false;
+        }
     }
 
     public Performer(int length, int idnum, String label) {
@@ -22,27 +30,80 @@ public class Performer {
         this.label = label;
         name = "defaultname";
         instrument = "defaultinstrument";
+
         posarray = new Point[length];
+        keyframe = new boolean[length];
+
+        for (int i = 0; i < length; i += 1) {
+            keyframe[i] = false;
+        }
     }
 
-    public Point getPos(int idx) {
-        // I'm not actually sure what .getLocation() does
-        // Java documentation says it "returns location"
-        // it may be more useful to write two seperate methods to get x and y
-        return posarray[idx].getLocation();
+    /*
+        default getter/setters
+    */
+
+    public void setid(int idnum) { this.idnum = idnum; }
+    public void setLabel(String label) { this.label = label; }
+    public void setName(String name) { this.name = name; }
+    public void setInst(String insttrument) { this.instrument = instrument; }
+    public void setPoint(int i, Point point) { posarray[i] = point; }
+    public void setKeyframe(int i, boolean bool) { keyframe[i] = bool; }
+
+    public int getid() { return idnum; }
+    public String getLabel() { return label; }
+    public String getName() { return name; }
+    public String getInst() { return instrument; }
+    public Point getPoint(int i) { return posarray[i]; }
+
+    public boolean isKeyframe(int i) {
+        return keyframe[i];
     }
 
-    public double getX(int idx) {
-        // returns x value of point at posarray idx
-        return posarray[idx].getX();
+    public int findCountsRight(int i) {
+        /*
+            finds counts (difference in index) between
+            indexed keyframe and subsequent keyframe
+
+            maybe not necessary to limit first argument to keyframes
+        */
+        if (i < 0) {
+            throw new IllegalArgumentException("index is negative");
+        }
+        else if (!isKeyframe(i)) {
+            throw new IllegalStateException("not a keyframe");
+        }
+        else {
+            for (int j = i + 1; j < keyframe.length; j += 1) {
+                if (keyframe[j]) {
+                    return j - i;
+                }
+            }
+            throw new IllegalStateException("no subsequent keyframe");
+        }
     }
 
-    public double getY(int idx) {
-        // returns y value of point array at posarray idx
-        return posarray[idx].getY();
-    }
+    public int findCountsLeft(int i) {
+        /*
+            finds counts between indexed keyframe
+            and previous keyframe
 
-    public void setPos(int idx, double x, double y) {
-        posarray[idx].setLocation(x, y);
+            again, maybe not necessary to limit to keyframes
+        */
+        if (i < 0) {
+            throw new IllegalArgumentException("index is negative");
+        }
+        else if (!isKeyframe(i)) {
+            throw new IllegalStateException("not a keyframe");
+        }
+        else {
+            for (int j = i - 1; j >= 0; j -= 1) {
+                if (keyframe[j]) {
+                    return i - j;
+                }
+            }
+            throw new IllegalStateException("no previous keyframe");
+        }
+
     }
 }
